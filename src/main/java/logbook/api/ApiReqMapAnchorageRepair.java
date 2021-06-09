db9b291b95f289c238c3fb40ac1d5921083790af
Sdk0815 <developer2@terumode.net>
2021-06-09 23:46:09 +0900

    緊急泊地修理の際にステータスを更新

---
+++ src/main/java/logbook/api/ApiReqMapAnchorageRepair.java
@@ -0,0 +1,37 @@
+package logbook.api;
+
+import java.util.Map;
+import java.util.Optional;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
+import logbook.bean.Ship;
+import logbook.bean.ShipCollection;
+import logbook.internal.JsonHelper;
+import logbook.proxy.RequestMetaData;
+import logbook.proxy.ResponseMetaData;
+
+/**
+ * /kcsapi/api_req_map/anchorage_repair
+ */
+@API("/kcsapi/api_req_map/anchorage_repair")
+public class ApiReqMapAnchorageRepair implements APIListenerSpi {
+
+    @Override
+    public void accept(JsonObject json, RequestMetaData req, ResponseMetaData res) {
+        Optional.ofNullable(json.getJsonObject("api_data"))
+            .map(data -> data.getJsonArray("api_ship_data"))
+            .ifPresent(this::apiShipData);
+    }
+    
+    /**
+     * api_data.api_ship_data
+     *
+     * @param array api_ship_data
+     */
+    private void apiShipData(JsonArray array) {
+        Map<Integer, Ship> map = ShipCollection.get().getShipMap();
+        map.putAll(JsonHelper.toMap(array, Ship::getId, Ship::toShip));
+    }
+}
