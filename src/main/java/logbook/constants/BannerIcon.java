06196173ec16659692bf2ece028f2f866935372b
sakura0689 <10571179+sakura0689@users.noreply.github.com>
2023-07-08 10:29:38 +0900

    [fix]2023.07.07 七夕アップデート対応

--- src/main/java/logbook/constants/BannerIcon.java
+++ src/main/java/logbook/constants/BannerIcon.java
@@ -15,25 +15,25 @@ public class BannerIcon {
     public static final String[] STANDING_DAMAGED = { "19.png", "19.jpg" };
 
     /** 小破バナーアイコン */
-    public static final String MC_BANNER_ICON0 = "common_misc/common_misc_110.png";
+    public static final String MC_BANNER_ICON0 = "common_misc/common_misc_112.png";
 
     /** 中破バナーアイコン */
     public static final String MC_BANNER_ICON1 = "common_misc/common_misc_104.png";
 
     /** 大破バナーアイコン */
-    public static final String MC_BANNER_ICON2 = "common_misc/common_misc_114.png";
+    public static final String MC_BANNER_ICON2 = "common_misc/common_misc_116.png";
 
     /** 撃沈バナーアイコン */
     public static final String MC_BANNER_ICON3 = "common_misc/common_misc_107.png";
 
     /** 修復バナーアイコン */
-    public static final String MC_BANNER_ICON4 = "common_misc/common_misc_113.png";
+    public static final String MC_BANNER_ICON4 = "common_misc/common_misc_115.png";
 
     /** 遠征バナーアイコン */
     public static final String MC_BANNER_ICON5 = "common_misc/common_misc_105.png";
 
     /** 退避バナーアイコン */
-    public static final String MC_BANNER_ICON10 = "common_misc/common_misc_115.png";
+    public static final String MC_BANNER_ICON10 = "common_misc/common_misc_117.png";
 
     /** 小破汚れ */
     public static final String MC_BANNER_SMOKE_IMG0 = "common_misc/common_misc_101.png";
@@ -48,12 +48,12 @@ public class BannerIcon {
     public static final String COMMON_MISC_35 = "common_misc/common_misc_39.png";
 
     /** 疲労オレンジ顔 */
-    public static final String COMMON_MISC_112 = "common_misc/common_misc_117.png";
+    public static final String COMMON_MISC_112 = "common_misc/common_misc_119.png";
 
     /** 疲労赤背景 */
     public static final String COMMON_MISC_36 = "common_misc/common_misc_40.png";
 
     /** 疲労赤顔 */
-    public static final String COMMON_MISC_113 = "common_misc/common_misc_118.png";
+    public static final String COMMON_MISC_113 = "common_misc/common_misc_120.png";
 
 }
