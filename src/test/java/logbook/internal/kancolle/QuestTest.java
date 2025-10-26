06a22469b18cff28c023edf61bd719ef389951e1
sakura0689 <10571179+sakura0689@users.noreply.github.com>
2025-10-26 13:54:53 +0900

    [fix]Config.getDefault関数のMock化対応

--- src/test/java/logbook/internal/kancolle/QuestTest.java
+++ src/test/java/logbook/internal/kancolle/QuestTest.java
@@ -1,11 +1,14 @@
 package logbook.internal.kancolle;
 
+import static org.mockito.Mockito.*;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 import logbook.bean.AppQuest;
 import logbook.bean.AppQuestCollection;
@@ -19,14 +22,18 @@ class QuestTest {
         Path testConfigPath = Paths.get("./target/test-classes/logbook/config");
         Config testConfig = new Config(testConfigPath);
         
-        AppQuestCollection appQuestCollection = testConfig.get(AppQuestCollection.class, AppQuestCollection::new);        
-        ConcurrentSkipListMap<Integer,AppQuest> questList = appQuestCollection.getQuest(); 
-        AppQuest quest = questList.get(1040);
-        AppQuestCondition condition = AppQuestCondition.loadFromResource(quest.getNo());
+        try (MockedStatic<Config> mocked = mockStatic(Config.class)) {
+            mocked.when(Config::getDefault).thenReturn(testConfig);
+            
+            AppQuestCollection appQuestCollection = AppQuestCollection.get();        
+            ConcurrentSkipListMap<Integer,AppQuest> questList = appQuestCollection.getQuest(); 
+            AppQuest quest = questList.get(1040);
+            AppQuestCondition condition = AppQuestCondition.loadFromResource(quest.getNo());
         
-        QuestCollect collect = QuestCollect.collect(quest, condition);
-        condition.test(collect);
-        Optional.of(condition);
+            QuestCollect collect = QuestCollect.collect(quest, condition);
+            condition.test(collect);
+            Optional.of(condition);
+        }
     }
 
 }
